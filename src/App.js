680fa93379ec54200e6fd3db40ce482287d632b5
Abhinav Sancheti <001sancheti@gmail.com>
2021-08-24 17:57:13 +0530

    added padding and text-wrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import IssueDetailsPage from './pages/issue-details-page.component';
 import { Route, Switch } from 'react-router-dom';
 import Header from './components/header/header.component.jsx';
 import NotFound from './components/not-found/not-found.component.jsx';
+import './App.css'
 
 function App() {
     return (
